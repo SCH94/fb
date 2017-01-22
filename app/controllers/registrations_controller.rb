0c511a54b481ae8e22487f269463f361880f3141
sic-f <sic-f@users.noreply.github.com>
2017-01-23 01:53:35 +0800

    Welcome mailer

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  def create
+    super
+    if resource.persisted?
+      UserMailer.welcome_email(resource).deliver_now
+    end
+  end
+
   protected
 
     def after_sign_up_path_for(resource)
