1cf2ca10d55cc727c04ffe75a41da64802484b88
sic-f <sic-f@users.noreply.github.com>
2017-01-23 00:25:10 +0800

    Signup Omniauth Facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
 
   validates :first_name, presence: true
 
@@ -34,5 +34,26 @@ class User < ApplicationRecord
     frienders = self.frienders
     friendees = fee.map(&:friendee)
     frienders + friendees
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.first_name = auth.info.name.split[0..-2].join(' ')
+      user.last_name = auth.info.name.split.last
+      user.username = auth.info.name.split.first.downcase
+    end
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+        user.first_name = data['name'].split[0..-2].join(' ')
+        user.last_name = data['name'].split.last
+        user.username = data['name'].split.first.downcase
+      end
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'registrations' }
+  devise_for :users, controllers: { registrations: 'registrations', omniauth_callbacks: 'omniauth_callbacks' }
   devise_scope :user do
     root 'static_pages#home'
   end
